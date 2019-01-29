9ec1cf527c15d278cda158013f3a13c25716250f
RomanSocial <rn.sergeewitch@gmail.com>
2019-01-29 16:25:19 +0200

    Добавил Magnific, адаптировал

--- newprovidence-new/js/common.js
+++ newprovidence-new/js/common.js
@@ -2,21 +2,50 @@ $(function () {
 
 	//  Гамбургер открывашка
 	$('.hamburger-toggle').click(function () {
-		var hamburgerWidth = parseInt($('.hamburger-menu__list').css('width'));
-
 		$(this).toggleClass('open');
-
-		if (hamburgerWidth === 0) {
-			$('.hamburger-menu__list').css('width', 250);
-		} else {
-			$('.hamburger-menu__list').css('width', 0);
-		};
+		$('.hamburger-menu__list').toggleClass('open');
 	});
 
 	// Кнопка в выезжающем меню
 	$('.hamburger-menu__closebtn').on('click', function () {
-		$('.hamburger-menu__list').css('width', 0);
+		$('.hamburger-menu__list').removeClass('open');
 		$('.hamburger-toggle').removeClass('open');
+	});
+
+	// Поп-ап окно с видео
+	$(document).ready(function () {
+		$('.main-screen__button').magnificPopup({
+			type: 'iframe',
+
+			iframe: {
+				markup: '<div class="mfp-iframe-scaler">' +
+					'<div class="mfp-close"></div>' +
+					'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+					'</div>',
+
+				patterns: {
+					youtube: {
+						index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+						id: 'v=', // String that splits URL in a two parts, second part should be %id%
+						// Or null - full URL will be returned
+						// Or a function that should return %id%, for example:
+						// id: function(url) { return 'parsed id'; }
+
+						src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
+					},
+				},
+				srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+			}
+		});
+		$('.pu-app__link').magnificPopup({
+			type: 'inline',
+			midClick: true
+		});
+		$('.pu-send__link').magnificPopup({
+			type: 'inline',
+			midClick: true
+		});
 	});
 
 	// Карусель отзывов
