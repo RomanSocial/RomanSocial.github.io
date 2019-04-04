604a101e2872ffe1365cc1d7fc3a173df66ac248
RomanSocial <rn.sergeewitch@gmail.com>
2019-04-04 23:37:46 +0300

    China Вёрстка

--- china/js/common.js
+++ china/js/common.js
@@ -1,5 +1,20 @@
-$(function() {
+$(function () {
 
-	// Custom JS
+	//Карусель отзывов
+	$('.c-slider').owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 10,
+		nav: true,
+		navContainer: ".c-slider__navigation",
+	})
+	var owl = $('.c-slider');
+	owl.owlCarousel();
+	$('.c-slider__prev').click(function () {
+		owl.trigger('prev.owl.carousel');
+	})
+	$('.c-slider__next').click(function () {
+		owl.trigger('next.owl.carousel');
+	})
 
 });
