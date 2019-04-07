bf45e14592cbb1aa4d487b29cd1f4c461c75a05f
RomanSocial <rn.sergeewitch@gmail.com>
2019-04-07 21:23:24 +0300

    Fir Вёрстка

--- fir/js/common.js
+++ fir/js/common.js
@@ -6,6 +6,12 @@ $(function() {
 		+ '<div  class="c-timer__item"><span class="c-timer__time">%M</span><span class="c-timer__text"> Мин</span></div>' 
 		+ '<div  class="c-timer__item"><span class="c-timer__time">%S</span><span class="c-timer__text"> Сек</span></div>'));
 	});
+	$('#sale-2nd-timer').countdown('2019/05/05', function(event) {
+		var $this = $(this).html(event.strftime(''
+		+ '<div class="c-timer__item"><span  class="c-timer__time">%H</span><span  class="c-timer__text"> Час</span></div>' 
+		+ '<div  class="c-timer__item"><span class="c-timer__time">%M</span><span class="c-timer__text"> Мин</span></div>' 
+		+ '<div  class="c-timer__item"><span class="c-timer__time">%S</span><span class="c-timer__text"> Сек</span></div>'));
+	});
 
 	$('.s-for').slick({
 		slidesToShow: 1,
@@ -18,8 +24,10 @@ $(function() {
 		slidesToShow: 5,
 		slidesToScroll: 1,
 		asNavFor: '.s-for',
-		dots: true,
+		dots: false,
 		centerMode: true,
+		prevArrow: $('.navigation__prev'),
+    nextArrow: $('.navigation__next'),
 		focusOnSelect: true
 	});
 
