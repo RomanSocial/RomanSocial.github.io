f9fd6a89fffc693e825339658665372f016583cf
RomanSocial <rn.sergeewitch@gmail.com>
2019-04-09 13:04:41 +0300

    China Вёрстка

--- china/js/common.js
+++ china/js/common.js
@@ -24,23 +24,60 @@ $(function () {
 		owl.trigger('next.owl.carousel');
 	})
 
-	$('#sale-1st-timer').countdown('2019/05/05', function(event) {
+	$('#sale-1st-timer').countdown('2019/05/05', function (event) {
 		var $this = $(this).html(event.strftime(''
-		+ '<div class="c-timer__item"><span  class="c-timer__time">%H</span><span  class="c-timer__text"> Час</span></div>' 
-		+ '<div  class="c-timer__item"><span class="c-timer__time">%M</span><span class="c-timer__text"> Мин</span></div>' 
-		+ '<div  class="c-timer__item"><span class="c-timer__time">%S</span><span class="c-timer__text"> Сек</span></div>'));
+			+ '<div class="c-timer__item"><span  class="c-timer__time">%H</span><span  class="c-timer__text"> Час</span></div>'
+			+ '<div  class="c-timer__item"><span class="c-timer__time">%M</span><span class="c-timer__text"> Мин</span></div>'
+			+ '<div  class="c-timer__item"><span class="c-timer__time">%S</span><span class="c-timer__text"> Сек</span></div>'));
 	});
-	$('#sale-2st-timer').countdown('2019/05/05', function(event) {
+	$('#sale-2st-timer').countdown('2019/05/05', function (event) {
 		var $this = $(this).html(event.strftime(''
-		+ '<div class="c-timer__item"><span  class="c-timer__time">%H</span><span  class="c-timer__text"> Час</span></div>' 
-		+ '<div  class="c-timer__item"><span class="c-timer__time">%M</span><span class="c-timer__text"> Мин</span></div>' 
-		+ '<div  class="c-timer__item"><span class="c-timer__time">%S</span><span class="c-timer__text"> Сек</span></div>'));
+			+ '<div class="c-timer__item"><span  class="c-timer__time">%H</span><span  class="c-timer__text"> Час</span></div>'
+			+ '<div  class="c-timer__item"><span class="c-timer__time">%M</span><span class="c-timer__text"> Мин</span></div>'
+			+ '<div  class="c-timer__item"><span class="c-timer__time">%S</span><span class="c-timer__text"> Сек</span></div>'));
 	});
-	$('#gift-1st-timer').countdown('2019/05/05', function(event) {
+	$('#gift-1st-timer').countdown('2019/05/05', function (event) {
 		var $this = $(this).html(event.strftime(''
-		+ '<div class="c-timer__item"><span  class="c-timer__time">%H</span><span  class="c-timer__text"> Час</span></div>' 
-		+ '<div  class="c-timer__item"><span class="c-timer__time">%M</span><span class="c-timer__text"> Мин</span></div>' 
-		+ '<div  class="c-timer__item"><span class="c-timer__time">%S</span><span class="c-timer__text"> Сек</span></div>'));
+			+ '<div class="c-timer__item"><span  class="c-timer__time">%H</span><span  class="c-timer__text"> Час</span></div>'
+			+ '<div  class="c-timer__item"><span class="c-timer__time">%M</span><span class="c-timer__text"> Мин</span></div>'
+			+ '<div  class="c-timer__item"><span class="c-timer__time">%S</span><span class="c-timer__text"> Сек</span></div>'));
+	});
+
+	// Поп-ап окно Форма
+	$('.open-popup-link').magnificPopup({
+		type: 'inline',
+		removalDelay: 500,
+		mainClass: 'mfp-fade',
+		midClick: true
+	});
+
+	// Плавный скрол
+	$('a[data-target^="anchor"]').bind('click.smoothscroll', function () {
+		var target = $(this).attr('href'),
+			bl_top = $(target).offset().top;
+		// bl_top = $(target).offset().top - 100; если нужен отступ, когда есть фикс меню
+		$('body, html').animate({ scrollTop: bl_top }, 700);
+		return false;
+	});
+
+	// Всплывающее видео
+	$('.ad-video__button').magnificPopup({
+		type: 'iframe',
+
+		iframe: {
+			markup: '<div class="mfp-iframe-scaler">' +
+				'<div class="mfp-close"></div>' +
+				'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+				'</div>',
+			patterns: {
+				youtube: {
+					index: 'youtube.com/',
+					id: 'v=',
+					src: '//www.youtube.com/embed/%id%?autoplay=1'
+				},
+			},
+			srcAction: 'iframe_src',
+		}
 	});
 
 });
