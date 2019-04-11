5b7f02f60608c5da831709411e67d63383ae8a5b
RomanSocial <rn.sergeewitch@gmail.com>
2019-04-11 12:35:27 +0300

    China Front-end

--- china/js/common.js
+++ china/js/common.js
@@ -7,6 +7,13 @@ $(function () {
 		$('.hamburger-menu').toggleClass('menu-open');
 	});
 
+	//  Телефо открывашка
+	$('.phone').click(function (e) {
+		e.preventDefault();
+		$('.phone__sec-phone').toggleClass('phone__sec-phone--open');
+		$('.phone__image').toggleClass('phone__image--open');
+	});
+
 	//Карусель отзывов
 	$('.c-slider').owlCarousel({
 		items: 1,
@@ -79,5 +86,290 @@ $(function () {
 			srcAction: 'iframe_src',
 		}
 	});
+
+	// Google Карта
+	var myLatlng = new google.maps.LatLng(55.617464, 37.715743);
+	var mapOptions = {
+		zoom: 17,
+		center: myLatlng
+	}
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	var marker = new google.maps.Marker({
+		position: myLatlng,
+		title: "RealOPT"
+	});
+	marker.setMap(map);
+
+	// Объявление попап-функций. Плагин Magnific Popup
+	var openSucsessPopup = function () {
+		$.magnificPopup.open({
+			items: { src: '#sucsess-popup' },
+			type: 'inline',
+			fixedContentPos: false,
+			fixedBgPos: true,
+			overflowY: 'auto',
+			closeBtnInside: true,
+			preloader: false,
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'mfp-fade'
+		});
+	};
+
+	var openFailPopup = function () {
+		$.magnificPopup.open({
+			items: { src: '#fail-popup' },
+			type: 'inline',
+			fixedContentPos: false,
+			fixedBgPos: true,
+			overflowY: 'auto',
+			closeBtnInside: true,
+			preloader: false,
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'mfp-fade'
+		});
+	};
+	// Функция сброса формы.
+	var resetForm = function () {
+		$('#sale-form-1').trigger('reset');
+	};
+	// Функция сброса формы.
+	var resetForm = function () {
+		$('#sale-form-2').trigger('reset');
+	};
+	// Функция сброса формы.
+	var resetForm = function () {
+		$('#sale-form-3').trigger('reset');
+	};
+
+	// Функции отслеживания событий
+	var submitListener = function () {
+		$('#sale-form-1').on("submit", sendAjax);
+	};
+
+	// Проверка форм
+	$('#sale-form-1').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2
+			}
+		},
+		messages: {
+			email: {
+				required: "Поле 'Email' обязательно к заполнению",
+				email: "Необходим формат адреса email"
+			},
+			text: "Поле обязательно к заполнению",
+		}
+	});
+	// Проверка форм
+	$('#sale-form-2').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2
+			}
+		},
+		messages: {
+			email: {
+				required: "Поле 'Email' обязательно к заполнению",
+				email: "Необходим формат адреса email"
+			},
+		}
+	});
+	// Проверка форм
+	$('#sale-form-3').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2
+			}
+		},
+		messages: {
+			email: {
+				required: "Поле 'Email' обязательно к заполнению",
+				email: "Необходим формат адреса email"
+			},
+		}
+	});
+
+	// Аяксовая отправка форм.
+	var sendAjax = function (event) {
+		event.preventDefault();
+		var submitButton = $('#sale-form-1').find('input[type="submit"]');
+		submitButton.attr('disabled', '');
+		var
+			form = $('#sale-form-1'),
+			url = form.attr('action'),
+			data = form.serialize(),
+			result = $.ajax({
+				url: "mailer/smart.php",
+				type: 'POST',
+				data: data
+			})
+				.done(function () {
+					console.log("sucsess");
+					openSucsessPopup();
+					resetForm();
+				})
+				.fail(function () {
+					console.log("error");
+					openFailPopup();
+				})
+				.always(function () {
+					submitButton.removeAttr('disabled');
+				});
+	};
+	// Аяксовая отправка форм.
+	var sendAjax = function (event) {
+		event.preventDefault();
+		var submitButton = $('#sale-form-2').find('input[type="submit"]');
+		submitButton.attr('disabled', '');
+		var
+			form = $('#sale-form-2'),
+			url = form.attr('action'),
+			data = form.serialize(),
+			result = $.ajax({
+				url: "mailer/smart.php",
+				type: 'POST',
+				data: data
+			})
+				.done(function () {
+					console.log("sucsess");
+					openSucsessPopup();
+					resetForm();
+				})
+				.fail(function () {
+					console.log("error");
+					openFailPopup();
+				})
+				.always(function () {
+					submitButton.removeAttr('disabled');
+				});
+	};
+	// Аяксовая отправка форм.
+	var sendAjax = function (event) {
+		event.preventDefault();
+		var submitButton = $('#sale-form-3').find('input[type="submit"]');
+		submitButton.attr('disabled', '');
+		var
+			form = $('#sale-form-3'),
+			url = form.attr('action'),
+			data = form.serialize(),
+			result = $.ajax({
+				url: "mailer/smart.php",
+				type: 'POST',
+				data: data
+			})
+				.done(function () {
+					console.log("sucsess");
+					openSucsessPopup();
+					resetForm();
+				})
+				.fail(function () {
+					console.log("error");
+					openFailPopup();
+				})
+				.always(function () {
+					submitButton.removeAttr('disabled');
+				});
+	};
+
+	// Инилизация функций.
+	submitListener();
+
+	// Оповещения о окончании времени
+	// 10 минут
+	var open10minPopup = function () {
+		$.magnificPopup.open({
+			items: { src: '#10min-timer' },
+			type: 'inline',
+			fixedContentPos: false,
+			fixedBgPos: true,
+			overflowY: 'auto',
+			closeBtnInside: true,
+			preloader: false,
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'mfp-fade'
+		});
+	};
+	// 5 минут
+	var open5minPopup = function () {
+		$.magnificPopup.open({
+			items: { src: '#5min-timer' },
+			type: 'inline',
+			fixedContentPos: false,
+			fixedBgPos: true,
+			overflowY: 'auto',
+			closeBtnInside: true,
+			preloader: false,
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'mfp-fade'
+		});
+	};
+	// 3 минут
+	var open3minPopup = function () {
+		$.magnificPopup.open({
+			items: { src: '#3min-timer' },
+			type: 'inline',
+			fixedContentPos: false,
+			fixedBgPos: true,
+			overflowY: 'auto',
+			closeBtnInside: true,
+			preloader: false,
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'mfp-fade'
+		});
+	};
+
+
+
+	// Таймер с подарком 
+	// $('#clock').countdown('2019/04/10 10:29:25')
+	// 	.on('update.countdown', function (event) {
+	// 		var format = '%H:%M:%S';
+	// 		if (event.offset.totalSeconds == 659) {
+	// 			open10minPopup();
+	// 		}
+	// 		if (event.offset.totalSeconds === 359) {
+	// 			open5minPopup();
+	// 		}
+	// 		if (event.offset.totalSeconds === 240) {
+	// 			open3minPopup();
+	// 		}
+	// 		if (event.offset.totalDays > 0) {
+	// 			format = '%-d day%!d ' + format;
+	// 		}
+	// 		if (event.offset.weeks > 0) {
+	// 			format = '%-w week%!w ' + format;
+	// 		}
+	// 		$(this).html(event.strftime(format));
+	// 	})
+	// 	.on('finish.countdown', function (event) {
+	// 		openFailPopup();
+	// 	});
+
+	// Задача: Взять время с сервера
+	// get the current time when button clicked
+	// var currentTime = moment();
+	// // add 25 minutes for interval
+	// var timerInterval = moment(currentTime).add(25, 's');
+	// // format interval
+	// var timerIntervalFormatted = moment(timerInterval).format('YYYY/MM/DD HH:mm:ss');
+	// // start the 25 minute timer and at the end popup two buttons for breaks
+	// $timer.removeClass('is-countdown').html('').countdown({
+	// 	until: timerInterval._d,
+	// 	onExpiry: function () {
+	// 		$timerButtons.toggleClass('break');
+	// 	}
+	// });
+
+
+
 
 });
