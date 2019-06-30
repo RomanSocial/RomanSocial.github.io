0fcae5ccade392592951b7663f1ff73741dbbc14
RomanSocial <rn.sergeewitch@gmail.com>
2019-06-30 23:05:39 +0300

    ArtSoft

---
+++ fir/js/common.js
@@ -0,0 +1,34 @@
+$(function() {
+
+	$('#sale-1st-timer').countdown('2019/05/05', function(event) {
+		var $this = $(this).html(event.strftime(''
+		+ '<div class="c-timer__item"><span  class="c-timer__time">%H</span><span  class="c-timer__text"> Час</span></div>' 
+		+ '<div  class="c-timer__item"><span class="c-timer__time">%M</span><span class="c-timer__text"> Мин</span></div>' 
+		+ '<div  class="c-timer__item"><span class="c-timer__time">%S</span><span class="c-timer__text"> Сек</span></div>'));
+	});
+	$('#sale-2nd-timer').countdown('2019/05/05', function(event) {
+		var $this = $(this).html(event.strftime(''
+		+ '<div class="c-timer__item"><span  class="c-timer__time">%H</span><span  class="c-timer__text"> Час</span></div>' 
+		+ '<div  class="c-timer__item"><span class="c-timer__time">%M</span><span class="c-timer__text"> Мин</span></div>' 
+		+ '<div  class="c-timer__item"><span class="c-timer__time">%S</span><span class="c-timer__text"> Сек</span></div>'));
+	});
+
+	$('.s-for').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.s-nav'
+	});
+	$('.s-nav').slick({ 
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		asNavFor: '.s-for',
+		dots: false,
+		centerMode: true,
+		prevArrow: $('.navigation__prev'),
+    nextArrow: $('.navigation__next'),
+		focusOnSelect: true
+	});
+
+});
