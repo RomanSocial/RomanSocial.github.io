38efcd26a99b23eeda56f5e9e6b1b70be5082bbd
RomanSocial <rn.sergeewitch@yandex.ru>
2021-01-08 14:08:35 +0200

    8/01/21
    1

--- fir/js/common.js
+++ fir/js/common.js
@@ -1,12 +1,12 @@
 $(function() {
 
-	$('#sale-1st-timer').countdown('2019/05/05', function(event) {
+	$('#sale-1st-timer').countdown('2021/05/05', function(event) {
 		var $this = $(this).html(event.strftime(''
 		+ '<div class="c-timer__item"><span  class="c-timer__time">%H</span><span  class="c-timer__text"> Час</span></div>' 
 		+ '<div  class="c-timer__item"><span class="c-timer__time">%M</span><span class="c-timer__text"> Мин</span></div>' 
 		+ '<div  class="c-timer__item"><span class="c-timer__time">%S</span><span class="c-timer__text"> Сек</span></div>'));
 	});
-	$('#sale-2nd-timer').countdown('2019/05/05', function(event) {
+	$('#sale-2nd-timer').countdown('2021/05/05', function(event) {
 		var $this = $(this).html(event.strftime(''
 		+ '<div class="c-timer__item"><span  class="c-timer__time">%H</span><span  class="c-timer__text"> Час</span></div>' 
 		+ '<div  class="c-timer__item"><span class="c-timer__time">%M</span><span class="c-timer__text"> Мин</span></div>' 
@@ -31,4 +31,9 @@ $(function() {
 		focusOnSelect: true
 	});
 
+	$("a[href*=#]:not([href=#])").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var t=$(this.hash);if(t=t.length?t:$("[name="+this.hash.slice(1)+"]"),t.length)return $("html,body").animate({scrollTop:t.offset().top},1000),!1}});
+
+
 });
+
+
